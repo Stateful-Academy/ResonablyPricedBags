83010535d7c92700eab73066e9a3e44466b34449
Karl Pfister <kpfister@me.com>
2023-07-04 11:36:38 -0600

    Complets List VIew Setup

---
+++ ResonablyPricedBags/ResonablyPricedBags/UI/BagTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BagTableViewCell.swift
+//  ResonablyPricedBags
+//
+//  Created by Karl Pfister on 7/4/23.
+//
+
+import UIKit
+
+class BagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
